a9cc8133ecbb5d3cea1ca41687ee24a63307ecd4
shubh2928 <theownershubh@gmail.com>
2025-03-06 04:17:46 +0530

    Update shubh.py

--- shubh.py
+++ shubh.py
@@ -4,8 +4,7 @@ import subprocess
 import datetime
 import os
 
-from keep_alive import keep_alive
-keep_alive()
+
 # insert your Telegram bot token here
 bot = telebot.TeleBot('7786469113:AAFjWtBSS24y3aVgEBFvcwbLNGHwcpQRg5g')
 
